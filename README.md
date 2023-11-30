2ff292ca99409c2f2b07021e8f3d05696dd24852
Kan <82136089+lkccrr@users.noreply.github.com>
2023-11-30 00:29:05 -0500

    0.0.1

---
+++ README.md
@@ -0,0 +1,21 @@
+[![spins2](https://img.shields.io/pypi/v/spins2?style=flat-square)](https://pypi.org/project/spins2/)
+[![spins2](https://img.shields.io/pypi/pyversions/spins2?style=flat-square)](https://pypi.org/project/spins2/)
+[![spins2](https://img.shields.io/pypi/l/spins2?style=flat-square)](https://pypi.org/project/spins2/)
+[![spins2](https://img.shields.io/pypi/dm/spins2?style=flat-square)](https://pypi.org/project/spins2/)
+[![spins2](https://img.shields.io/pypi/wheel/spins2?style=flat-square)](https://pypi.org/project/spins2/)
+[![spins2](https://img.shields.io/github/last-commit/lkccrr/spins2?style=flat-square)](https://github.com/lkccrr/spins2)
+[![spins2](https://img.shields.io/github/release-date/lkccrr/spins2?style=flat-square)](https://github.com/lkccrr/spins2)
+
+### spins2
+
+The <b style="color:green;"><i>spins2</b></i> is A Monte Carlo Simulation Code for the Phase Transition in 2D/3D Materials.
+***
+<b style="color:blue;"><i>spins2</b></i>
+* To execute <b style="color:blue;"><i>spins2</b></i> <b style="color:red;"><i>\-h</b></i> for the parameters to use.
+* Example:
+```bash
+spins2 -h
+spins2 -n 4 -x 200 -y 200 -e 200 -w 1000 -t 35 -r
+spins2 -x 200 -y 200 -t 0 32 4 33 40 1 40.2 45 0.2 46 55 1 -e 200 -w 1000 -r
+```
+
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from spins2 import __version__
+
+setup(
+    name='spins2',
+    version=__version__,
+    author='kan',
+    author_email='luo_kan+pypi@outlook.com',
+    python_requires=">=3.6",
+    license='MIT',
+    license_files=('LICENSE.txt',),
+    platforms=['Unix', 'Windows'],
+    keywords='Monte Carlo Simulation Ising Model',
+    description='spins2: A Monte Carlo Simulation Code for the Phase Transition in 2D/3D Materials',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/lkccrr/spins2',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3 :: Only",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12'
+    ],
+    install_requires=[
+        'numba>=0.54.0',
+        'numpy>=1.22.0',
+        'matplotlib>=3.4.0'
+    ],
+    entry_points={
+        'console_scripts': ['spins2=spins2.wrapper:main']
+    },
+    packages=find_packages(),
+    include_package_data=True
+)
